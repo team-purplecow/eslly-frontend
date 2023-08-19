d6be7f88d90ba518fe923edd8580aabbf55aff5c
Sn-Kinos <dony435@naver.com>
2023-08-19 16:15:24 +0900

    feat: make components folder

--- src/components/Common/Header/Header.styled.ts
+++ src/components/Common/Header/Header.styled.ts
@@ -1,5 +1,8 @@
 import styled from '@emotion/styled';
+import Image from 'next/image';
 
 export const HeaderContainer = styled.header`
   height: 136px;
 `;
+
+export const LogoBox = styled(Image)``;
---
+++ src/components/Common/Header/Header.tsx
@@ -0,0 +1,10 @@
+import { HeaderContainer, LogoBox } from './Header.styled';
+import tempLogo from 'Assets/';
+
+export const Header = () => {
+  return (
+    <HeaderContainer>
+      <LogoBox src={} alt={''} />
+    </HeaderContainer>
+  );
+};
