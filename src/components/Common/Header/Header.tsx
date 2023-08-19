ee0766dea4f679c30e5ec0a2119ce0b112a39c31
Sn-Kinos <dony435@naver.com>
2023-08-19 21:48:13 +0900

    feat: header component publishing

--- src/components/Common/Header/Header.tsx
+++ src/components/Common/Header/Header.tsx
@@ -1,10 +1,33 @@
-import { HeaderContainer } from './Header.styled';
+import { Logout, Search } from '@mui/icons-material';
+import { SvgIcon } from '@mui/material';
+import { useRef } from 'react';
+import {
+  HeaderContainer,
+  HeaderLogoutLink,
+  HeaderSearchBox,
+  HeaderSearchInput,
+  LogoBox,
+} from './Header.styled';
 
 export const Header = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
     <HeaderContainer>
-      {/* <LogoBox src={} alt={''} /> */}
-      here is header
+      <LogoBox>ESLLY</LogoBox>
+      <HeaderSearchBox
+        onClick={() => {
+          inputRef.current.focus();
+          inputRef.current.select();
+        }}
+      >
+        <SvgIcon component={Search} sx={{ fontSize: 32 }} />
+        <HeaderSearchInput ref={inputRef} placeholder='참여자 이름을 검색해주세요' />
+      </HeaderSearchBox>
+      <HeaderLogoutLink href={'/logout'}>
+        <SvgIcon component={Logout} sx={{ fontSize: 32 }} />
+        Logout
+      </HeaderLogoutLink>
     </HeaderContainer>
   );
 };
