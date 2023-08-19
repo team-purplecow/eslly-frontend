3b68b073c66f013268c3b8527fe4e86d48236995
seolyeongbae <peixueying@gmail.com>
2023-08-20 06:06:53 +0900

    feat: implement nametag and bingo

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 
+const withImages = require('next-images');
+
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
     emotion: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = withImages({
+  webpack(config, options) {
+    return config;
+  },
+});
+
+module.exports = nextConfig;
